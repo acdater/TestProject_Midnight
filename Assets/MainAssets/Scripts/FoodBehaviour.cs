89f83035c8a6f6c03597d3749e946800e20f0635
acdater <cegley25@gmail.com>
2023-10-14 23:24:36 +0300

    Food burst
    Added some simple prefab of eaten food.

--- Assets/MainAssets/Scripts/FoodBehaviour.cs
+++ Assets/MainAssets/Scripts/FoodBehaviour.cs
@@ -4,11 +4,15 @@ using UnityEngine;
 
 public class FoodBehaviour : MonoBehaviour
 {
+    [SerializeField] private GameObject _burstPRefab;
+
     private void OnTriggerEnter(Collider other)
     {
         if (other.gameObject.CompareTag("Fish"))
         {
+            var effect = Instantiate(_burstPRefab, this.gameObject.transform.position, Quaternion.identity);
             Destroy(gameObject);
+            Destroy(effect, 2.0f);
         }
     }
 }
